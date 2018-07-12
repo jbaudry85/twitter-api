3c6d1dd0365d225dc6449fa81999ae22e9285928
jbaudry@gmail.com <jbaudry@gmail.com>
2018-07-12 14:44:25 +0200

    blabla

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,10 @@
-# app/__init.py__
-from flask import Flask
+# app/__init__.py
+from flask import Flask # This line already exists
+
+from .db import tweet_repository
+from .models import Tweet
+tweet_repository.add(Tweet("a first tweet"))
+tweet_repository.add(Tweet("a second tweet"))
 
 def create_app():
     app = Flask(__name__)
@@ -7,5 +12,8 @@ def create_app():
     # Remove the previous code using `@app` and replace it with:
     from .main.controllers import main
     app.register_blueprint(main)
+
+    from .api.tweets import api as tweet_api
+    app.register_blueprint(tweet_api, url_prefix = "/api/v1")
 
     return app
---
+++ app/models.py
@@ -0,0 +1,8 @@
+#app/models.py
+from datetime import datetime
+
+class Tweet:
+    def __init__(self, text):
+        self.text = text
+        self.created_at = datetime.now()
+        self.id = None
